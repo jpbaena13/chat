5c8e020dd2872d691464e33e16a377f2633e6dbb
Juan Pablo <jpbaena13@gmail.com>
2017-11-29 11:26:18 -0500

    Fist

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var port = 8080;
+
+app.set('views', __dirname + '/tpl');
+app.set('view engine', 'jade');
+app.engine('jade', require('jade').__express);
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req,res) {
+	res.sendFile(path.join(__dirname + '/index.html'));
+})
+
+var io = require('socket.io').listen(app.listen(port));
+
+var socketIDs = [];
+
+io.sockets.on('connection', function(socket) {
+	socketIDs.push(socket.id);
+
+	io.sockets.emit('connected', { sockets: socketIDs });
+
+	socket.on('send', function(data) {
+		io.sockets.emit('message', data);
+	});
+
+	socket.on('disconnect', function(data) {
+		socketIDs.splice( socketIDs.indexOf(socket.id) ); 
+		io.sockets.emit('disconnected', {id: socket.id} );
+	})
+});
