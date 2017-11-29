bca3fba7aac8d4207668e6647477e01fc38171f6
Juan Pablo <jpbaena13@gmail.com>
2017-11-29 12:40:28 -0500

    Ready

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
-var app = require('express')()
-	,https = require('http').Server(app)
-	,io = require('socket.io')(https);
-
-app.get('/', function(req, res) {
-	res.send('Lantia Realtime :)');
-});
-
+var express = require('express');
+var app = express();
+var path = require('path');
 var port = process.env.port || 8080;
 
-https.listen(port);
+app.set('views', __dirname + '/tpl');
+app.set('view engine', 'jade');
+app.engine('jade', require('jade').__express);
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req,res) {
+	res.sendFile(path.join(__dirname + '/tpl/index.html'));
+})
+
+var io = require('socket.io').listen(app.listen(port));
+
+var socketIDs = [];
+
+io.sockets.on('connection', function(socket) {
+	socketIDs.push(socket.id);
+
+	io.sockets.emit('connected', { sockets: socketIDs });
+
+	socket.on('send', function(data) {
+		io.sockets.emit('message', data);
+	});
+
+	socket.on('disconnect', function(data) {
+		socketIDs.splice( socketIDs.indexOf(socket.id) ); 
+		io.sockets.emit('disconnected', {id: socket.id} );
+	})
+});
